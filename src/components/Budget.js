7f1e312b22292f52fa7238d00706fc01d7031bfe
Jack Wong <jackckw001@gmail.com>
2023-05-30 06:04:30 -0400

    changes made during the final exam step 1 to 3

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,54 @@
-
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
+    //const [budget, setBudget] = useState('');
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const refreshBudget = (budget) => {
+
+        if(budget > 20000) {
+            alert("The budget value cannot exceed £20,000.");
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: 20000
+            });
+            return;
+        }
+
+        if(budget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending.");
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: totalExpenses
+            });
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget
+        });
+
+    }
+
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+            <input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => refreshBudget(event.target.value)}>
+                        </input>
+                        
+            </span>
         </div>
     );
 };
